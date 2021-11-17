292f4852e919b768a606912f0b9d28b0d70b1313
luke_perrin <lukeperrin27@gmail.com>
2021-11-17 14:47:38 +0100

    sets up basic next.js appliction with footer component

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import Footer from '../components/footer/footer'
+import '../styles/style.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp() {
+  return  (
+    <container className='page-container'>
+      <Footer />
+    </container>
+  )
 }
 
 export default MyApp
---
+++ public/assets/data/staffCards.js
@@ -0,0 +1,37 @@
+import Jens from '../images/staff_photos/Jens.png'
+import Luke from '../images/staff_photos/Luke.png'
+import Kim from '../images/staff_photos/Kim.png'
+import Thomas from '../images/staff_photos/Thomas.png'
+
+const staffCards = [
+  {
+    id: 1,
+    name: 'Jens Østgaard',
+    image: Jens,
+    description:
+      'As a designer educated in Florens, I thrive in the midst of art, people, strong culture, technology and business. Please don’t ask me about sport. I once asked Olof Mellberg if his father was Ola Serneke. I aspire to become a skilled logomaker, and want to create the best Co-work in town to speed up the succes of others!',
+  },
+  {
+    id: 2,
+    name: 'Luke Perrin',
+    image: Luke,
+    description:
+      'As a full-stack developer to deliver the best product possible requires accuracy, precision, focus and a fair amount of patience. Professional bug hunter and planning guru. Former mediocre rugby player, so I know my fair share of sport related topics, so ask me and not Jens.',
+  },
+  {
+    id: 3,
+    name: 'Kim Haaga',
+    image: Kim,
+    description:
+      'I am a former tinsmith and probably worlds best worst golfer. Craftsmanship at heart and making the clients happy is most important to me and what keeps me going',
+  },
+  {
+    id: 4,
+    name: 'Thomas Ochman',
+    image: Thomas,
+    description:
+      'Fullstack developer with a passion for turning ideas and dreams into code and building systems that connects people and business. Devoted agile-practitioner and evangelist. Open source contributor.',
+  },
+]
+
+export default staffCards
