2fdc081e1b39586de790ebe00ccda196c7fa7474
kim_haaga <kim.haaga@live.se>
2021-11-18 16:00:22 +0100

    Add content to hero

--- layout/Hero.jsx
+++ layout/Hero.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
 
 const Hero = () => {
   return (
-    <div className="hero">
-      Hero
-    </div>
+    <section className="hero">
+
+      <div className="bg-video">
+        <video className="bg-video__content" autoPlay muted loop>
+        <source src='/assets/videos/HeroMediumBitRate.mp4' type='video/mp4' />
+        Your browser is not supported
+        </video>
+      </div>
+      <div className="u-center-text hero__header">
+        <h1 className="heading">Digital transformation is a game changer <br></br>We help you Adapt</h1>
+      </div>
+      <div className="hero__btn">
+        <button className="btn">
+          get in touch
+        </button>
+      </div>
+    </section>
   )
 }
 
