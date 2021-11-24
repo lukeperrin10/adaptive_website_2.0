e2ac85c783c09db1d3bbccb0b77a4b5e7448b8d8
luke <lukeperrin27@gmail.com>
2021-11-24 15:10:17 +0100

    implements some content to contact layout adds some styling

--- layout/Contact.jsx
+++ layout/Contact.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
   return (
     <div className="contact">
-      Contact
+      <h2 className="heading-2">{"< contact us />"}</h2>
+      <div className="contact__contact-information">
+        <div className="contact__get-in-touch">
+          <h4 className="heading-4">get in touch</h4>
+          <p>
+            To Know more about what we can offer you, please get in touch. Feel
+            free to send us an email, a message on LinkedIn or use the form. We
+            will be happy to discuss your requirements and present possible
+            solutions.
+          </p>
+          <h4 className='heading-6'>Email - info@adaptivelabs.se</h4>
+        </div>
+        <div className="contact__form">
+          <h4 className="heading-4">send us a message</h4>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
