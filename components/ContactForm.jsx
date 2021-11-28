a8ab31604936cfff17e1629adafbf0a5dafe3800
kim_haaga <kim.haaga@live.se>
2021-11-28 13:13:30 +0100

    Fix warning in contact get in touch section link, Fix contact form

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -1,48 +1,63 @@
-import React from "react";
+import React from 'react'
 
 const ContactForm = () => {
   return (
-    <form name="contact" method="POST" data-netlify="true">
-      <input type="hidden" name="contact-us" value="contact" />
-      <div className="contact__contact-form-group">
+    <form name='contact' method='POST' data-netlify='true' className='form'>
+      <div className='form__group'>
         <input
-          className="contact__form-input"
-          type="text"
-          name="name"
-          autoComplete="off"
-          placeholder="Name"
+          className='form__input'
+          type='text'
+          name='name'
+          autoComplete='off'
+          placeholder='Full Name'
         />
-
+        <label htmlFor='name' className='form__label'>
+          Full Name
+        </label>
+      </div>
+      <div className='form__group'>
         <input
-          className="contact__form-input"
-          type="email"
-          name="email"
-          autoComplete="off"
-          placeholder="Email"
+          className='form__input'
+          type='email'
+          name='email'
+          autoComplete='off'
+          placeholder='Email'
         />
-
+        <label htmlFor='email' className='form__label'>
+          Email
+        </label>
+      </div>
+      <div className='form__group'>
         <input
-          className="contact__form-input"
-          type="tel"
-          name="phone"
-          autoComplete="off"
-          placeholder="Phone"
+          className='form__input'
+          type='tel'
+          name='phone'
+          autoComplete='off'
+          placeholder='Phone'
         />
-
-        <input
-          className="contact__form-text-area"
-          type="textarea"
-          name="message"
-          autoComplete="off"
-          placeholder="Message"
+        <label htmlFor='phone' className='form__label'>
+          Phone
+        </label>
+      </div>
+      <div className='form__group'>
+        <textarea
+          className='form__input form__input--text-area'
+          type='textarea'
+          name='message'
+          autoComplete='off'
+          placeholder='Message'
         />
-
-        <button className="btn" type="submit">
+        <label htmlFor='message' className='form__label'>
+          Message
+        </label>
+      </div>
+      <div className='form__group'>
+        <button className='btn' type='submit'>
           Submit
         </button>
       </div>
     </form>
-  );
-};
+  )
+}
 
-export default ContactForm;
+export default ContactForm
